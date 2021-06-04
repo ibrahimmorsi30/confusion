c26cd7ffb5cd73c696ca1dcc67fef26bdaddde7f
ibrahim morsi <ibrahim.com2011@gmail.com>
2021-06-05 01:03:04 +0200

    functional components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Main />
       </div>
     );
   }
 }
-
 export default App;
